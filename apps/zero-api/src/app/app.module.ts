b2b3eadfad081d56cc363751c881da489c925678
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-06-29 21:18:43 +0200

    chore: PrismaModule

--- apps/zero-api/src/app/app.module.ts
+++ apps/zero-api/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: ['.env'],
-    isGlobal: true,
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true
+    }),
+    PrismaModule
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService]
 })
 export class AppModule {}
