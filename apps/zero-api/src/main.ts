577367290abc176ad16dd13a64b2475b2dfb1951
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-06-29 20:43:14 +0200

    chore: OAS3 documentation

--- apps/zero-api/src/main.ts
+++ apps/zero-api/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -12,6 +13,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Energy Web Zero API')
+    .setVersion('0.3')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document, { customSiteTitle: 'Swagger documentation for Energy Web Zero API' });
+
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
