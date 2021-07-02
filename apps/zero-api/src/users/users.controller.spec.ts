63256eca0b156e5824b4a4dece2e4ba5e568d721
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-07-02 15:29:10 +0200

    test: user.controller, users.service

--- apps/zero-api/src/users/users.controller.spec.ts
+++ apps/zero-api/src/users/users.controller.spec.ts
@@ -1,21 +1,123 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+
+import * as request from 'supertest';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { AppModule } from '../app/app.module';
+import { UserEntity } from './entities/user.entity';
 
 describe('UsersController', () => {
+  let app: INestApplication;
   let controller: UsersController;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
+  const validPayload: CreateUserDto = {
+    name: 'test name',
+    email: 'test-email@foo.bar',
+    password: 'a secret'
+  };
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [UsersService, PrismaService],
+      imports: [AppModule]
     }).compile();
 
+    app = module.createNestApplication();
+    await app.init();
+
     controller = module.get<UsersController>(UsersController);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  afterAll(async () => {
+    await app?.close();
+  });
+
+  beforeEach(async () => {
+    await prisma.user.deleteMany();
+    expect((await prisma.user.findMany()).length).toEqual(0);
+  });
+
+  afterEach(async () => {
+    await prisma.user.deleteMany();
+    expect((await prisma.user.findMany()).length).toEqual(0);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should accept valid payload and create a user', async function() {
+    const res = await request(app.getHttpServer())
+      .post('/users')
+      .send(validPayload)
+      .expect(HttpStatus.CREATED);
+
+    expect(res).toBeDefined();
+
+    const userCreated = res.body as Omit<UserEntity, 'password'>;
+
+    expect(userCreated.id).toBeDefined();
+    expect(userCreated.name).toEqual(validPayload.name);
+    expect(userCreated.email).toEqual(validPayload.email);
+
+    const dbRecord = await prisma.user.findUnique({ where: { id: userCreated.id } });
+    expect(dbRecord).toBeDefined();
+    expect(dbRecord.name).toEqual(validPayload.name);
+    expect(dbRecord.email).toEqual(validPayload.email);
+  });
+
+  it('should not accept payloads with invalid email', async function() {
+    const payload: CreateUserDto = { ...validPayload, email: 'this is not a valid email' };
+
+    await request(app.getHttpServer())
+      .post('/users')
+      .send(payload)
+      .expect(HttpStatus.BAD_REQUEST);
+
+    expect((await prisma.user.findMany()).length).toEqual(0);
+  });
+
+  it('should not accept payloads with an empty password', async function() {
+    const payload: CreateUserDto = { ...validPayload, password: '' };
+
+    await request(app.getHttpServer())
+      .post('/users')
+      .send(payload)
+      .expect(HttpStatus.BAD_REQUEST);
+
+    expect((await prisma.user.findMany()).length).toEqual(0);
+  });
+
+  it('should not accept payloads with an empty name', async function() {
+    const payload: CreateUserDto = { ...validPayload, name: '' };
+
+    await request(app.getHttpServer())
+      .post('/users')
+      .send(payload)
+      .expect(HttpStatus.BAD_REQUEST);
+
+    expect((await prisma.user.findMany()).length).toEqual(0);
+  });
+
+  it('should respond with correct user record', async function() {
+    const res = await request(app.getHttpServer())
+      .post('/users')
+      .send(validPayload)
+      .expect(HttpStatus.CREATED);
+
+    const userCreated = res.body as Omit<UserEntity, 'password'>;
+
+    const userFetched = (await request(app.getHttpServer())
+      .get(`/users/${userCreated.id}`)
+      .send(validPayload)
+      .expect(HttpStatus.OK)).body as Omit<UserEntity, 'password'>;
+
+    expect(userCreated.email).toEqual(userFetched.email);
   });
 });
--- apps/zero-api/src/users/users.service.spec.ts
+++ apps/zero-api/src/users/users.service.spec.ts
@@ -1,9 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as bcrypt from 'bcryptjs';
 import { UsersService } from './users.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 describe('UsersService', () => {
   let service: UsersService;
+  const testData1: CreateUserDto = {
+    name: 'test name1',
+    email: 'test-email1@foo.bar',
+    password: 'test password 1'
+  };
+  const testData2: CreateUserDto = {
+      name: 'test name2',
+      email: 'test-email2@foo.bar',
+      password: 'test password 2'
+    };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,9 +23,53 @@ describe('UsersService', () => {
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+
+    await Promise.all((await service.findAll()).map(async (user) => await service.remove(user.id)))
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create user', async function() {
+    const newUser = await service.create(testData1);
+
+    expect(newUser).toBeDefined();
+    expect(newUser.id).toBeDefined();
+
+    const newUserFetched = await service.findOne(newUser.id)
+
+    expect(newUserFetched.email).toEqual(testData1.email);
+  });
+
+  it('should fetch all users created', async function() {
+    const u1 = await service.create(testData1);
+    const u2 = await service.create(testData2);
+
+    const users = await service.findAll();
+
+    expect(users.length).toEqual(2);
+    expect(u1.email).toEqual(testData1.email);
+    expect(u2.email).toEqual(testData2.email);
+  });
+
+  it('should create record with hashed password', async function() {
+    const newUser = await service.create(testData1);
+    expect(newUser.password).not.toEqual(testData1.password);
+    expect(bcrypt.compare(testData1.password, newUser.password));
+  });
+
+  it('email address should be unique', async function() {
+    await service.create(testData1);
+
+    await service.create({...testData2, email: testData1.email}).then(() => {
+      throw('Promise should be rejected');
+    }).catch((err) => {
+      expect(err).toBeDefined();
+      expect(err).toHaveProperty('code')
+      expect(err).toHaveProperty('stack')
+      expect(err).toHaveProperty('message')
+      expect(err.code).toEqual('P2002');
+    })
   });
 });
