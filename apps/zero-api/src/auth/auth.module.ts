9cfd089a51b2aa1c0a4112e01d84d25fa7e366d1
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-07-08 10:41:56 +0200

    refactor: strategies folder

--- apps/zero-api/src/auth/auth.module.ts
+++ apps/zero-api/src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule} from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './local.strategy';
+import { LocalStrategy } from './strategies';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
