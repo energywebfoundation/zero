2b5cbb9a864787e22a9ec23a26034b29ffdead7c
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-07-09 12:03:37 +0200

    test: refactor

--- apps/zero-api/src/users/users.controller.spec.ts
+++ apps/zero-api/src/users/users.controller.spec.ts
@@ -12,6 +12,7 @@ import { UserRole } from '@prisma/client';
 
 describe('UsersController', () => {
   let app: INestApplication;
+  let httpServer;
   let controller: UsersController;
   let prisma: PrismaService;
 
@@ -32,6 +33,8 @@ describe('UsersController', () => {
 
     app = module.createNestApplication();
     await app.init();
+
+    httpServer = app.getHttpServer();
 
     controller = module.get<UsersController>(UsersController);
     prisma = module.get<PrismaService>(PrismaService);
@@ -168,18 +171,13 @@ describe('UsersController', () => {
 
       expect(newUser).toBeDefined();
 
-      const accessToken = (await request(app.getHttpServer())
-        .post('/auth/login')
-        .send({
-          username: validPayload.email,
-          password: validPayload.password
-        })
-        .expect(HttpStatus.OK)).body.accessToken;
+      const accessToken = await logInUser(app, validPayload.email, validPayload.password);
+      expect(accessToken).toBeDefined();
 
-      const updateResponseBody = (await request(app.getHttpServer())
+      const updateResponseBody = (await request(httpServer)
         .patch(`/users/${newUser.id}`)
         .send({ email: 'modified@email.com' })
-        .set('Authorization', `Bearer ${accessToken}`)
+        .set(getAuthBearerHeader(accessToken))
         .expect(HttpStatus.OK)).body;
 
       expect(updateResponseBody.email).not.toEqual('modified@email.com');
@@ -197,13 +195,13 @@ describe('UsersController', () => {
     })
 
     beforeEach(async function() {
-      newSellerUserEmail = (await request(app.getHttpServer())
+      newSellerUserEmail = (await request(httpServer)
         .post('/users')
         .send({ ...validPayload, email: 'seller@foobar.com', roles: [UserRole.seller] })
         .expect(HttpStatus.CREATED)).body.email;
       expect(newSellerUserEmail).toBeDefined();
 
-      newAdminUserEmail = (await request(app.getHttpServer())
+      newAdminUserEmail = (await request(httpServer)
         .post('/users')
         .send({ ...validPayload, email: 'admin@foobar.com', roles: [UserRole.admin] })
         .expect(HttpStatus.CREATED)).body.email;
@@ -211,33 +209,34 @@ describe('UsersController', () => {
     });
 
     it('should respond if logged-in user has a required role', async function() {
-      const accessToken: string = (await request(app.getHttpServer())
-        .post('/auth/login')
-        .send({
-          username: newAdminUserEmail,
-          password: validPayload.password
-        })
-        .expect(HttpStatus.OK)).body.accessToken;
+      const accessToken = await logInUser(app, newAdminUserEmail, validPayload.password);
+      expect(accessToken).toBeDefined();
 
-      await request(app.getHttpServer())
+      await request(httpServer)
         .get('/users')
-        .set('Authorization', `Bearer ${accessToken}`)
+        .set(getAuthBearerHeader(accessToken))
         .expect(HttpStatus.OK);
     });
 
     it('should deny access if logged-in user does not have a required role', async function() {
-      const accessToken: string = (await request(app.getHttpServer())
-          .post('/auth/login')
-          .send({
-            username: newSellerUserEmail,
-            password: validPayload.password
-          })
-          .expect(HttpStatus.OK)).body.accessToken;
+      const accessToken = await logInUser(app, newSellerUserEmail, validPayload.password);
+      expect(accessToken).toBeDefined();
 
-      await request(app.getHttpServer())
+      await request(httpServer)
         .get('/users')
-        .set('Authorization', `Bearer ${accessToken}`)
+        .set(getAuthBearerHeader(accessToken))
         .expect(HttpStatus.FORBIDDEN);
     });
   });
 });
+
+async function logInUser(app: INestApplication, username: string, password: string): Promise<string> {
+  return (await request(app.getHttpServer())
+    .post('/auth/login')
+    .send({ username, password })
+    .expect(HttpStatus.OK)).body.accessToken
+}
+
+function getAuthBearerHeader(token: string): {Authorization : string} {
+  return {Authorization: `Bearer ${token}`}
+}
