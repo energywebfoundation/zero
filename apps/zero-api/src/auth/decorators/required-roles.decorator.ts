7d11126c587a6ff6f536254bdfe26f974bc23d52
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-07-09 12:30:14 +0200

    refactor: renamed Roles decorator to RequiredRoles

--- apps/zero-api/src/auth/decorators/required-roles.decorator.ts
+++ apps/zero-api/src/auth/decorators/required-roles.decorator.ts
@@ -2,4 +2,4 @@ import { SetMetadata } from '@nestjs/common';
 import { UserRole } from '@prisma/client';
 
 export const ROLES_KEY = 'roles';
-export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
+export const RequiredRoles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
