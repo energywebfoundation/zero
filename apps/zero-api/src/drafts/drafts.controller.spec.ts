a30f012c8020491023c7a4966688c5345db20631
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-07-13 18:42:25 +0200

    chore: fixed tests teardown

--- apps/zero-api/src/drafts/drafts.controller.spec.ts
+++ apps/zero-api/src/drafts/drafts.controller.spec.ts
@@ -4,16 +4,21 @@ import { DraftsService } from './drafts.service';
 import { PrismaService } from '../prisma/prisma.service';
 
 describe('DraftsController', () => {
+  let module: TestingModule;
   let controller: DraftsController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
       controllers: [DraftsController],
       providers: [DraftsService, PrismaService],
     }).compile();
 
     controller = module.get<DraftsController>(DraftsController);
   });
+
+  afterAll(async () => {
+    await module.close();
+  })
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
--- apps/zero-api/src/drafts/drafts.service.spec.ts
+++ apps/zero-api/src/drafts/drafts.service.spec.ts
@@ -3,15 +3,20 @@ import { DraftsService } from './drafts.service';
 import { PrismaService } from '../prisma/prisma.service';
 
 describe('DraftsService', () => {
+  let module: TestingModule;
   let service: DraftsService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
       providers: [DraftsService, PrismaService],
     }).compile();
 
     service = module.get<DraftsService>(DraftsService);
   });
+
+  afterAll(async () => {
+    await module.close();
+  })
 
   it('should be defined', () => {
     expect(service).toBeDefined();
