b0de796a1ae02afa9ef4c74a89ffa20aa2a4caf2
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-07-14 12:48:14 +0200

    chore: removed DraftsController

--- apps/zero-api/src/drafts/drafts.module.ts
+++ apps/zero-api/src/drafts/drafts.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DraftsService } from './drafts.service';
-import { DraftsController } from './drafts.controller';
 
 @Module({
-  controllers: [DraftsController],
   providers: [DraftsService]
 })
 export class DraftsModule {}
