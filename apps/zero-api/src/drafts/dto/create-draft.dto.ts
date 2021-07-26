6b69b37b7da405be24cc2f01c5b35ad94866d282
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-07-27 01:18:41 +0200

    fix(zero-api): no draftType field validation

--- apps/zero-api/src/drafts/dto/create-draft.dto.ts
+++ apps/zero-api/src/drafts/dto/create-draft.dto.ts
@@ -1,10 +1,12 @@
 import { DraftType, Prisma } from '@prisma/client';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum } from 'class-validator';
 
 export class CreateDraftDto {
   @ApiProperty()
   data: Prisma.JsonValue;
 
   @ApiProperty({ enum: DraftType, enumName: 'DraftType' })
+  @IsEnum(DraftType)
   draftType: DraftType;
 }
