f02668ae87c51940b929d15d24952e614e73be45
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-09-29 14:44:46 +0200

    chore(backend): removed all /api/users/me/files base path endpoints

--- apps/zero-api/src/users/users.module.ts
+++ apps/zero-api/src/users/users.module.ts
@@ -5,10 +5,9 @@ import { UsersDraftsController } from './users-drafts.controller';
 import { UsersFilesController } from './users-files.controller';
 import { DraftsModule } from '../drafts/drafts.module';
 import { FilesModule } from '../files/files.module';
-import { UsersOwnFilesController } from './users-own-files.controller';
 
 @Module({
-  controllers: [UsersController, UsersDraftsController, UsersOwnFilesController, UsersFilesController],
+  controllers: [UsersController, UsersDraftsController, UsersFilesController],
   imports: [DraftsModule, FilesModule],
   providers: [UsersService],
   exports: [UsersService]
