8e5355f2066880b9c14d5281f925ef7d5d38258f
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-10-14 14:54:56 +0200

    fix(backend): test failing because of prisma service not available

--- apps/zero-api/src/healthckeck/healthckeck.service.spec.ts
+++ apps/zero-api/src/healthckeck/healthckeck.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthckeckService } from './healthckeck.service';
+import { AppModule } from '../app/app.module';
 
 describe('HealthckeckService', () => {
   let service: HealthckeckService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HealthckeckService]
+      imports: [AppModule]
     }).compile();
 
     service = module.get<HealthckeckService>(HealthckeckService);
